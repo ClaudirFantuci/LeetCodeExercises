2c152a08a42f6dfbfea8fbebd02ab610c8b44fc2
Claudir Fantuci Junior <cladirfantcijunior@gmail.com>
2025-06-05 00:06:23 -0300

    add| add two numbers

--- AddTwoNumbers/Solution.java
+++ AddTwoNumbers/Solution.java
@@ -11,28 +11,33 @@ package AddTwoNumbers;
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode l3 = head;
         int carry = 0;
-        while(l1 != 0 || l2 != 0){
-            int l1Value = (l1!=null) ? l1.val : 0;
-            int l2Value = (l2!=null) ? l2.val : 0;
+        while (l1 != 0 || l2 != 0) {
+            int l1Value = (l1 != null) ? l1.val : 0;
+            int l2Value = (l2 != null) ? l2.val : 0;
 
             int sum = l1Value + l2Value + carry;
-            carry = sum /10;
+            carry = sum / 10;
             int number = sum % 10;
             ListNode newNode = new ListNode(number);
             l3.next = newNode;
-            if(l1 != null){l1 = l1.next;}
-            if(l2 != null){l2 = l2.next;}
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
             l3 = l3.next;
         }
-        if(carry > 0){
+        if (carry > 0) {
             ListNode newNode = new ListNode(number);
             l3.next = newNode;
             l3 = l3.next;
         }
 
         return l3.next;
+    }
 }
